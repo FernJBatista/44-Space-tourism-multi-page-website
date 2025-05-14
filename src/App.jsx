58eaf6c0bcfe1ece416338f4498975e37e9a0e82
Fernando Batista <fernjbatista@gmail.com>
2025-05-14 15:18:11 +0200

    Updated Home page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
 import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Destination from './pages/Destination'
-import Crew from './pages/Crew'
-import Technology from './pages/Technology'
+import Home from './Pages/Home'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/destination" element={<Destination />} />
-      <Route path="/crew" element={<Crew />} />
-      <Route path="/technology" element={<Technology />} />
-    </Routes>
+    <Home />
   )
 }
 
---
+++ src/Components/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Navigation from '../UI/Navigation'
+
+const Header = () => {
+    return (
+        <div className='
+        relative flex flex-row w-full items-center justify-start gap-12 pl-12 mt-12
+        md:gap-8
+        '>
+            <img src="../public/assets/shared/logo.svg" alt="logo" className='w-12 h-12' />
+            <div className='
+                h-[1px] z-10 bg-gray-700
+                md:w-[10%]
+                lg:w-[32%]
+                '>
+            </div>
+            <Navigation />
+        </div>
+
+    )
+}
+
+export default Header
