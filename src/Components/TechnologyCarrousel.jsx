d23abb09ac0f764070e0a0e289f7dc771668a548
Fernando Batista <fernjbatista@gmail.com>
2025-05-21 15:42:44 +0200

    Update. Finished set up for technology page.

--- src/Components/TechnologyCarrousel.jsx
+++ src/Components/TechnologyCarrousel.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useEffect, useState } from 'react'
 import { AnimatePresence, motion } from 'framer-motion';
+import { div } from 'framer-motion/client';
 
 const TechnologyCarrousel = () => {
 
@@ -18,15 +19,42 @@ const TechnologyCarrousel = () => {
         if (!data) {
             return <div>Loading...</div>;
         }
+
+        console.log(data.technology)
     
-        const activeTechnology = data.crew[technology]
+        const activeTechnology = data.technology[technology]
 
     return (
         // Carrousel Container
-        <div>
-            {/* Carrousel Numbers */}
-            <div className="flex flex-col gap-4">
-                
+        <div className='flex flex-row h-full items-center gap-16 mt-4'>
+            {/* Carrousel content */}
+            <div className="flex flex-col gap-6 items-center"> 
+                {/* Technology Selector */}
+                {data.technology.map((_,index) => (
+                    <button
+                    key={index}
+                    onClick={() => setTechnology(index)}
+                    className={`
+                    w-16 h-16 rounded-full border-2 text-2xl font-bold font-bellefair 
+                    ${technology === index ? 'bg-white text-black' : 'bg-transparent text-gray-400 border-gray-700'}
+                    transition duration-300 ease-in-out
+                    `}
+                    >
+                        {index + 1}
+                    </button>
+                ))}
+            </div>
+            {/* Technology Text */}
+            <div>
+                <div className='flex flex-col gap-4 mr-16'>
+                    <h1 className='uppercase text-lg text-gray-500 tracking-widest'>The Technology...</h1>
+                    <h2 className='uppercase font-bellefair text-5xl'>{activeTechnology.name}</h2> 
+                    <p className='text-lg text-gray-400 tracking-wider max-w-[50ch]'>{activeTechnology.description}</p>
+                </div>
+            </div>
+            {/* Technology Image */}
+            <div className="justify-self-end">
+                <img src={activeTechnology.images.portrait} alt={activeTechnology.name} />
             </div>
         </div>
     )
--- src/pages/Technology.jsx
+++ src/pages/Technology.jsx
@@ -3,12 +3,12 @@ import TechnologyCarrousel from '../Components/TechnologyCarrousel'
 
 const Technology = () => {
     return (
-        <section className='flex flex-row w-full h-full gap-12 pl-12 mt-12 max-w-[72rem]'>
+        <section className='flex flex-col w-full h-full pl-12 mt-12 max-w-[90rem]'>
             {/* Technology Container */}
             <img src="../Public\assets\technology\background-technology-desktop.jpg" alt="technology-bg" className='absolute top-0 left-0 w-full h-full object-cover z-[-1]' />
             {/* Title */}
-            <div className="uppercase flex flex-row gap-4 text-3xl text-gray-200 tracking-widest mt-12">
-                <span className='text-3xl text-gray-500 tracking-widest'>03</span>Space Launch 101 
+            <div className="uppercase flex flex-row gap-4 text-3xl text-gray-200 tracking-widest mt-12 mb-6">
+                <span className='text-gray-500 tracking-widest'>03</span>Space Launch 101 
             </div>
             <main>
                 <TechnologyCarrousel />
